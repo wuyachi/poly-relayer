48bf348f0aafea63c3da673f8de82b70a203a393
wuyachi <wuyachi@163.com>
2022-12-06 14:40:57 +0800

    Add Ontology chain

--- relayer/relayer.go
+++ relayer/relayer.go
@@ -20,6 +20,7 @@ package relayer
 import (
 	"context"
 	"fmt"
+	"github.com/polynetwork/poly-relayer/relayer/ont"
 	"github.com/polynetwork/poly-relayer/relayer/ontevm"
 	"sync"
 	"time"
@@ -77,6 +78,8 @@ func GetListener(chain uint64) (listener IChainListener) {
 		listener = new(ok.Listener)
 	case base.ZION:
 		listener = new(po.Listener)
+	case base.ONT:
+		listener = new(ont.Listener)
 	case base.ONTEVM:
 		listener = new(ontevm.Listener)
 	default:
